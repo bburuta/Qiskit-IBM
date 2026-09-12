111df3484bfd50dce26960c3cb60a4610aafe79e
bburuta <bburutaran001@gmail.com>
2026-09-12 02:40:06 +0200

    c141 - New experiment batteries for GPU 21

--- slurm/submit_train.sh
+++ slurm/submit_train.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Resubmit only the jobs moved away from node03 CPU and the H100 GPU.
+# Resubmit only the three pending q16 noiseless REG GPU jobs on node14 CPUs.
 # Cancel first:
-# scancel 1582445 1582444 1582438 1582442 1582443 1582434
-sbatch slurm/slurm.sbatch_train_conv_remaining_cpu_045_node14
-sbatch slurm/slurm.sbatch_train_conv_remaining_cpu_065_node15
-sbatch slurm/slurm.sbatch_train_conv_remaining_cpu_066_node14
-sbatch slurm/slurm.sbatch_train_conv_remaining_cpu_092_node15
-sbatch slurm/slurm.sbatch_train_conv_remaining_cpu_093_node14
-sbatch slurm/slurm.sbatch_train_conv_remaining_cpu_081_node15
+# scancel 1582435 1582436 1582437
+sbatch slurm/slurm.sbatch_train_conv_remaining_cpu_107_node14
+sbatch slurm/slurm.sbatch_train_conv_remaining_cpu_108_node14
+sbatch slurm/slurm.sbatch_train_conv_remaining_cpu_109_node14
